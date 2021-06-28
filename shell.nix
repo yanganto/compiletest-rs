852e7a3c60b3c2d9e1be885a7b11b0f61dc3819c
Antonio Yang <yanganto@gmail.com>
2021-06-28 14:54:28 +0800

    Add nix shell

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  mozillaOverlay =
+    import (builtins.fetchGit {
+      url = "https://github.com/mozilla/nixpkgs-mozilla.git";
+      rev = "57c8084c7ef41366993909c20491e359bbb90f54";
+    });
+  nixpkgs = import <nixpkgs> { overlays = [ mozillaOverlay ]; };
+  rust-stable = with nixpkgs; ((rustChannelOf { date = "2021-05-06"; channel = "stable"; }).rust.override { });
+in
+with nixpkgs; pkgs.mkShell {
+  buildInputs = [
+    rust-stable
+  ];
+}
